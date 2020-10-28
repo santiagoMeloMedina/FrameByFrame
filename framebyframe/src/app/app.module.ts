c7d51204af8f2fd7e9f07de7a3c00b6653d81568
santiago <smelomedina05@gmail.com>
2020-10-27 19:05:09 -0500

    separated sections

--- framebyframe/src/app/app.module.ts
+++ framebyframe/src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FeedComponent } from './component/feed/feed.component';
 import { MenuComponent } from './component/menu/menu.component';
+import { FrontComponent } from './component/feed/front/front.component';
+import { AboutComponent } from './component/feed/about/about.component';
+import { WorkComponent } from './component/feed/work/work.component';
+import { ContactComponent } from './component/feed/contact/contact.component';
+import { ClientsComponent } from './component/feed/clients/clients.component';
+import { InformationComponent } from './component/feed/information/information.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FeedComponent,
-    MenuComponent
+    MenuComponent,
+    FrontComponent,
+    AboutComponent,
+    WorkComponent,
+    ContactComponent,
+    ClientsComponent,
+    InformationComponent
   ],
   imports: [
     BrowserModule,
